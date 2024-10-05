3e60c13c378681c8ba30ad868510793674897a20
Juan Diego Mora Rubio <juanjuo37@gmail.com>
2024-10-04 23:12:59 -0400

    Added extra audio classes

---
+++ sproj_2024/audio/Record.cpp
@@ -0,0 +1,30 @@
+//
+// Created by Juan Diego on 10/4/24.
+//
+
+#include "Record.h"
+
+Record::Record()
+{
+
+}
+
+Record::~Record()
+{
+
+}
+
+void Record::startRecording()
+{
+
+}
+
+void Record::stopRecording()
+{
+
+}
+
+void Record::cropRecording()
+{
+
+}
---
+++ sproj_2024/audio/Record.h
@@ -0,0 +1,25 @@
+//
+// Created by Juan Diego on 10/4/24.
+//
+
+#ifndef RECORD_H
+#define RECORD_H
+
+#endif //RECORD_H
+
+//#include <juce_audio_buffer/juce_audio_buffer.h>
+//#include <juce_audio_source/juce_audio_source.h>
+
+class Record
+{
+public:
+
+    Record();
+    ~Record();
+
+    void startRecording();
+    void stopRecording();
+    void cropRecording();
+
+};
+
